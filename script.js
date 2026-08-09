eb1bc1fb00b19b3d91a1d46cf514d811b626ad11
Stephen Somtochukwu <anyanwustevo18@gmail.com>
2026-08-09 16:38:16 +0100

    Update portfolio SEO and website

--- script.js
+++ script.js
@@ -55,7 +55,7 @@ const observer = new IntersectionObserver((entries) => {
 
 
 // --- Typing/Deleting Effect for Roles ---
-const roles = ["Frontend Web Developer", "Graphic Designer", "Microsoft Office Specialist", "Web Designer"];
+const roles = ["Full-Stack Web Developer", "Web Designer", "Graphic Designer", "Microsoft Office Specialist"];
 let roleIndex = 0;
 let charIndex = 0;
 let isDeleting = false;
